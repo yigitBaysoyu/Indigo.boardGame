6d10a030af55b92cddbbacc9362d4b573be71e4c
kusaynaeem <94133560+kusaynaeem@users.noreply.github.com>
2023-12-14 01:15:51 +0100

    The AbstractRefreshingService.kt and RootService.kt (for PlayerService.kt and GameService.kt) has been implemented. The class Refreshable.kt is empty

---
+++ src/main/kotlin/service/RootService.kt
@@ -0,0 +1,39 @@
+package service
+
+import entity.*
+import view.*
+
+/**
+ * Main class of the service layer for the War card game. Provides access
+ * to all other service classes and holds the [currentGame] state for these
+ * services to access.
+ */
+
+
+class RootService {
+    val gameService = GameService(this)
+    val playerService = PlayerService(this)
+
+    /**
+     * The currently active game. Can be `null`, if no game has started yet.
+     */
+    var currentGame : IndigoGame? = null
+
+    /**
+     * Adds the provided [newRefreshable] to all services connected
+     * to this root service
+     */
+    fun addRefreshable(newRefreshable: Refreshable) {
+        gameService.addRefreshable(newRefreshable)
+        playerService.addRefreshable(newRefreshable)
+    }
+
+    /**
+     * Adds each of the provided [newRefreshables] to all services
+     * connected to this root service
+     */
+    fun addRefreshables(vararg newRefreshables: Refreshable) {
+        newRefreshables.forEach { addRefreshable(it) }
+    }
+
+}
