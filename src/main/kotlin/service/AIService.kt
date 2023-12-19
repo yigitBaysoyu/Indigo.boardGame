bfb7980c6425e7329944029ca54807e701ebdfb6
johannesheinrich <johannes.heinrich@tu-dortmund.de>
2023-12-19 15:12:00 +0100

    Implemented/Changed AIService.randomNextTurn to find a valid position and keep track of knowdledge of possible positions to shorten time finding new one in the instance

--- src/main/kotlin/service/AIService.kt
+++ src/main/kotlin/service/AIService.kt
@@ -1,6 +1,9 @@
 package service
 
+import entity.EmptyTile
+import entity.PathTile
 import entity.PlayerType
+import entity.Tile
 import kotlin.random.Random
 
 class AIService(private val rootService: RootService) {
@@ -9,23 +12,57 @@ class AIService(private val rootService: RootService) {
 
     fun randomNextTurn() {
         val gameService = rootService.gameService
+        val playerService = rootService.playerService
         val currentGame = rootService.currentGame
         checkNotNull(currentGame)
+        //check(gameService.isGameEnded())
 
         val player = currentGame.getActivePlayer()
         require(player.playerType == PlayerType.RANDOMAI)
 
-        require(placeableTiles.isNotEmpty())
         placeableTiles.shuffle()
 
-        var selectedTile = placeableTiles.removeFirst()
-
-        while (!gameService.isPlaceable(selectedTile.first, selectedTile.second, player.playHand)){
-            selectedTile = placeableTiles.removeFirst()
+        //Rotate the tile by a random amount
+        val randomRotation = Random.Default.nextInt(0, 6)
+        for (i in 0 until randomRotation){
+            playerService.rotateTile(player.playHand)
         }
 
-        val randomRotation = Random.Default.nextInt(0, 6)
+        var selectedPos: Pair<Int,Int> ?= null
+        var selectedTile: Tile
 
+        while (placeableTiles.isNotEmpty()){
+            selectedPos = placeableTiles.first()
+            selectedTile = gameService.getTileFromAxialCoordinates(selectedPos.first, selectedPos.second)
+
+            if(selectedTile is PathTile){
+                placeableTiles.removeFirst()
+                continue
+            }
+            else if(gameService.isPlaceable(selectedPos.first, selectedPos.second, player.playHand)){
+                break
+            }
+            //If isPlaceable returns false for an empty position it means that the tile blocks a exit
+            //Then a rotation will always solve it given the existing tile types
+            else if(selectedTile is EmptyTile){
+                playerService.rotateTile(player.playHand)
+                continue
+            }
+        }
+
+        //No possible move for the AI meaning game has ended
+        if(placeableTiles.isEmpty()){
+            gameService.endGame()
+            return
+        }
+
+        checkNotNull(selectedPos)
+        gameService.setTileFromAxialCoordinates(selectedPos.first, selectedPos.second, player.playHand)
+        placeableTiles.removeFirst()
+
+        //TODO: Turn object is still missing, also no method which moves gems after move
+        //TODO: Cant call gameService.placeTile() since private
+        //TODO: Documentation and testing
     }
 
     private fun initializePlaceableTiles(){
