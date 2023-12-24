71e59517bac8de6e22fcb94fc042a2eef0095257
kusaynaeem <94133560+kusaynaeem@users.noreply.github.com>
2023-12-24 22:57:54 +0100

    the IsPlaceAbleTest test and the startNewGameFour test were modified to simplify the complexity.

--- src/test/kotlin/service/IsPlaceAbleTest.kt
+++ src/test/kotlin/service/IsPlaceAbleTest.kt
@@ -27,11 +27,6 @@ class IsPlaceAbleTest {
         val tile1 = PathTile(mutableMapOf(1 to 2 , 3 to 4 , 5 to 0),0,0,
             0, mutableListOf())
 
-        //Check if a tile can be placed outside the allowed space
-        assertFalse(game.gameService.isPlaceAble(5,1,tile))
-        assertFalse(game.gameService.isPlaceAble(-5,1,tile))
-        assertFalse(game.gameService.isPlaceAble(1,-5,tile))
-        assertFalse(game.gameService.isPlaceAble(1,5,tile))
 
         //Check if a tile can be placed on centerTile or a treasurerTile
         assertFalse(game.gameService.isPlaceAble(0,0, tile))
@@ -86,11 +81,6 @@ class IsPlaceAbleTest {
             0, mutableListOf())
 
 
-        //Check if other positions are valid.
-        assertTrue(game.gameService.isPlaceAble(2,1,tile1))
-        assertTrue(game.gameService.isPlaceAble(1,2,tile1))
-        assertTrue(game.gameService.isPlaceAble(-2,1,tile1))
-        assertTrue(game.gameService.isPlaceAble(1,-2,tile1))
         //check if a tile that do not have connections from 4 to 5 or form 5 to 4 and on the positions
         assertTrue(game.gameService.isPlaceAble(-1,-3,tile1))
         assertTrue(game.gameService.isPlaceAble(-2,-2,tile1))
@@ -113,6 +103,57 @@ class IsPlaceAbleTest {
 
         //check if a tile that do not have connections from 1 to 0 or form 0 to 1 and y = 4
         assertTrue(game.gameService.isPlaceAble(-1,4,tile1))
+
+    }
+
+    /**
+     * Tests the `isPlaceAble` method for various scenarios where tile placement should be invalid.
+     * Ensures the method correctly identifies and prohibits illegal placements.
+     */
+    @Test
+    fun isPlaceAbleOutsideTest(){
+        val game = RootService()
+        val player = mutableListOf(
+            Player("q",0, PlayerType.LOCALPLAYER,0, 0, mutableListOf(), mutableListOf())
+            , Player("q1",0, PlayerType.LOCALPLAYER,0, 0, mutableListOf(), mutableListOf())
+        )
+        game.gameService.startNewGame( player , false,1.0,false)
+        val tile = PathTile(mutableMapOf(0 to 1 , 2 to 3 , 4 to 5 ),0,0,
+            0, mutableListOf())
+
+        //Check if a tile can be placed outside the allowed space
+        assertFalse(game.gameService.isPlaceAble(5,1,tile))
+        assertFalse(game.gameService.isPlaceAble(-5,1,tile))
+        assertFalse(game.gameService.isPlaceAble(1,-5,tile))
+        assertFalse(game.gameService.isPlaceAble(1,5,tile))
+
+    }
+
+    /**
+     * Tests the `isPlaceAble` method for scenarios where tile placement should be valid.
+     * Checks that the method correctly allows legal tile placements.
+     */
+    @Test
+    fun isPlaceAbleInsideTest() {
+        val game = RootService()
+        val player = mutableListOf(
+            Player("q", 0, PlayerType.LOCALPLAYER, 0, 0, mutableListOf(), mutableListOf()),
+            Player("q1", 0, PlayerType.LOCALPLAYER, 0, 0, mutableListOf(), mutableListOf())
+        )
+        game.gameService.startNewGame(player, false, 1.0, false)
+
+
+        val tile1 = PathTile(
+            mutableMapOf(1 to 2, 3 to 4, 5 to 0), 0, 0,
+            0, mutableListOf()
+        )
+
+
+        //Check if other positions are valid.
+        assertTrue(game.gameService.isPlaceAble(2, 1, tile1))
+        assertTrue(game.gameService.isPlaceAble(1, 2, tile1))
+        assertTrue(game.gameService.isPlaceAble(-2, 1, tile1))
+        assertTrue(game.gameService.isPlaceAble(1, -2, tile1))
 
     }
 }
