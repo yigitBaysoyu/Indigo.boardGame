c354379806d2cfcb980c27ef11848ef3a9c82865
Alexander Kleyn <alex@deminator.de>
2024-01-05 13:59:37 +0100

    added header, added remove buttons, aligned everything

--- src/main/kotlin/view/StartGameScene.kt
+++ src/main/kotlin/view/StartGameScene.kt
@@ -6,6 +6,7 @@ import tools.aqua.bgw.components.container.Area
 import tools.aqua.bgw.components.gamecomponentviews.TokenView
 import tools.aqua.bgw.components.layoutviews.Pane
 import tools.aqua.bgw.components.uicomponents.Button
+import tools.aqua.bgw.components.uicomponents.Label
 import tools.aqua.bgw.components.uicomponents.TextField
 import tools.aqua.bgw.core.MenuScene
 import tools.aqua.bgw.util.Font
@@ -23,8 +24,8 @@ class StartGameScene(private val rootService: RootService) : MenuScene(Constants
     private val sceneWidth = Constants.SCENE_WIDTH
     private  val sceneHeight = Constants.SCENE_HEIGHT
     private val halfWidth = sceneWidth / 2
-    private val offsetY = 200
-    private val offsetX = 0
+    private val offsetY = 250
+    private val offsetX = 50
 
     private val cornersBackground = Button(
         posX = 0, posY = 0,
@@ -34,13 +35,20 @@ class StartGameScene(private val rootService: RootService) : MenuScene(Constants
         isDisabled = true
     }
 
+    private val headerLabel = Label(
+        width = 500, height = 100,
+        posX = halfWidth - 500 / 2, posY = 75,
+        text = "Local Game",
+        font = Font(size = 65, fontWeight = Font.FontWeight.BOLD, color = Color(250, 250, 240))
+    )
+
     private val playerNameInputList = mutableListOf<TextField>().apply {
         for(i in 0 until 4) {
             val playerNameInput: TextField = TextField(
-                width = 350, height = 50,
-                posX = halfWidth - 350 / 2 + offsetX, posY = 100*i + offsetY,
+                width = 500, height = 75,
+                posX = halfWidth - 500 / 2 + offsetX, posY = 150*i + offsetY,
                 prompt = "name ...",
-                font = Font(size = 30, Color(0, 0, 0)),
+                font = Font(size = 35, Color(0, 0, 0)),
                 visual = Visual.EMPTY
             ).apply {
                 componentStyle = "-fx-background-color: #fafaf0; -fx-background-radius: 25px;"
@@ -52,22 +60,22 @@ class StartGameScene(private val rootService: RootService) : MenuScene(Constants
     private val playerModeIconList = mutableListOf<Pane<Button>>().apply {
         for (i in 0 until 4) {
             val pane = Pane<Button>(
-                posX = 1225 + offsetX, posY = i*100 + offsetY + 2,
-                width = 50, height = 50,
+                posX = halfWidth - 450 + offsetX, posY = i*150 + offsetY + 2,
+                width = 75, height = 75,
                 visual = Visual.EMPTY
             )
 
             val playerModeIconBackground = Button(
-                width = 50, height = 50,
+                width = 75, height = 75,
                 posX = 0, posY = 0,
                 visual = Visual.EMPTY
             ).apply {
-                componentStyle = "-fx-background-color: #ffffffff; -fx-background-radius: 15px;"
+                componentStyle = "-fx-background-color: #ffffffff; -fx-background-radius: 25px;"
             }
 
             val playerModeIcon = Button(
-                width = 40, height = 40,
-                posX = 5, posY = 5,
+                width = 60, height = 60,
+                posX = 7, posY = 7,
                 visual = ImageVisual(Constants.modeIconPlayer)
             ).apply {
                 isDisabled = true
@@ -87,22 +95,22 @@ class StartGameScene(private val rootService: RootService) : MenuScene(Constants
         )
         for (i in 0 until 4) {
             val pane = Pane<Button>(
-                posX = 1150 + offsetX, posY = i*100 + offsetY + 2,
-                width = 50, height = 50,
+                posX = halfWidth - 350 + offsetX, posY = i*150 + offsetY + 2,
+                width = 75, height = 75,
                 visual = Visual.EMPTY
             )
 
             val playerColorIconBackground = Button(
-                width = 50, height = 50,
+                width = 75, height = 75,
                 posX = 0, posY = 0,
                 visual = Visual.EMPTY
             ).apply {
-                componentStyle = "-fx-background-color: #ffffffff; -fx-background-radius: 15px;"
+                componentStyle = "-fx-background-color: #ffffffff; -fx-background-radius: 25px;"
             }
 
             val playerColorIcon = Button(
-                width = 40, height = 40,
-                posX = 5, posY = 5,
+                width = 60, height = 60,
+                posX = 8, posY = 8,
                 visual = colorIconList[i]
             ).apply {
                 isDisabled = true
@@ -113,9 +121,40 @@ class StartGameScene(private val rootService: RootService) : MenuScene(Constants
         }
     }
 
+    private val playerRemoveIconList = mutableListOf<Pane<Button>>().apply {
+        for (i in 0 until 4) {
+            val pane = Pane<Button>(
+                posX = halfWidth + 275 + offsetX, posY = i*150 + offsetY + 2,
+                width = 75, height = 75,
+                visual = Visual.EMPTY
+            )
+
+            val playerRemoveIconBackground = Button(
+                width = 75, height = 75,
+                posX = 0, posY = 0,
+                visual = Visual.EMPTY
+            ).apply {
+                componentStyle = "-fx-background-color: #ff999900; -fx-background-radius: 25px;"
+            }
+
+            val playerRemoveIcon = Button(
+                width = 60, height = 60,
+                posX = 8, posY = 8,
+                visual = ImageVisual(Constants.minusIcon)
+            ).apply {
+                isDisabled = true
+            }
+            pane.add(playerRemoveIconBackground)
+            pane.add(playerRemoveIcon)
+            add(pane)
+        }
+    }
+
+
+
     private val addPlayerButton = Button(
         width = 50, height = 50,
-        posX = halfWidth - 50 / 2, posY = 400 + offsetY,
+        posX = halfWidth - 50 / 2, posY = 2*150 + offsetY,
         text = "",
         visual = ImageVisual(Constants.plusIcon)
     ).apply {
@@ -124,7 +163,7 @@ class StartGameScene(private val rootService: RootService) : MenuScene(Constants
 
     private val startButton = Button(
         width = 350, height = 75,
-        posX = halfWidth + 15, posY = 650 + offsetY,
+        posX = halfWidth + 15, posY = 900,
         text = "Start",
         font = Font(size = 45, fontWeight = Font.FontWeight.BOLD, color = Color(250, 250, 240)),
         visual = Visual.EMPTY
@@ -134,7 +173,7 @@ class StartGameScene(private val rootService: RootService) : MenuScene(Constants
 
     private val backButton = Button(
         width = 350, height = 75,
-        posX = halfWidth - 350 - 15, posY = 650 + offsetY,
+        posX = halfWidth - 350 - 15, posY = 900,
         text = "Back",
         font = Font(size = 45, fontWeight = Font.FontWeight.BOLD, color = Color(250, 250, 240)),
         visual = Visual.EMPTY
@@ -147,6 +186,7 @@ class StartGameScene(private val rootService: RootService) : MenuScene(Constants
 
         addComponents(
             cornersBackground,
+            headerLabel,
             playerNameInputList[0],
             playerNameInputList[1],
             playerNameInputList[2],
@@ -159,6 +199,10 @@ class StartGameScene(private val rootService: RootService) : MenuScene(Constants
             playerColorIconList[1],
             playerColorIconList[2],
             playerColorIconList[3],
+            playerRemoveIconList[0],
+            playerRemoveIconList[1],
+            playerRemoveIconList[2],
+            playerRemoveIconList[3],
             addPlayerButton,
             backButton,
             startButton
