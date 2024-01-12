a0e9947ab29a13b5a0aaf5bfbb817e1aaab088fd
David Flores Mitrovic <109731560+dFloresMitrovic@users.noreply.github.com>
2024-01-12 21:18:00 +0100

    delete space

--- src/main/kotlin/view/StartGameScene.kt
+++ src/main/kotlin/view/StartGameScene.kt
@@ -22,7 +22,7 @@ import java.awt.Color
 class StartGameScene(private val rootService: RootService) : MenuScene(Constants.SCENE_WIDTH, Constants.SCENE_HEIGHT), Refreshable {
 
     private val sceneWidth = Constants.SCENE_WIDTH
-    private  val sceneHeight = Constants.SCENE_HEIGHT
+    private val sceneHeight = Constants.SCENE_HEIGHT
     private val halfWidth = sceneWidth / 2
     private val offsetY = 250
     private val offsetX = 50
