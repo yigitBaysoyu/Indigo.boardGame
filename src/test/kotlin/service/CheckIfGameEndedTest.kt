5ef6cc22b8a9856b14b676f50d9e1afcf9012454
kusaynaeem <94133560+kusaynaeem@users.noreply.github.com>
2024-01-13 02:25:21 +0100

    the checkIfGameEnded Test has been fixed

--- src/test/kotlin/service/CheckIfGameEndedTest.kt
+++ src/test/kotlin/service/CheckIfGameEndedTest.kt
@@ -1,13 +1,11 @@
 package service
 
-import entity.GemType
-import entity.PathTile
-import entity.Player
-import entity.PlayerType
+import entity.*
 import org.junit.jupiter.api.assertThrows
 import kotlin.test.Test
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
+
 
 /**
  * This test class verifies the functionality of the `CheckIfGameEnded` method in the game service.
@@ -16,10 +14,10 @@ import kotlin.test.assertTrue
 class CheckIfGameEndedTest {
 
     /**
-     * Tests if the game ends correctly when all the gems have reached the gates.
+     * Tests if the game ends correctly when all the gems have removed from the field.
      */
     @Test
-    fun testCheckIfGameEnded(){
+    fun testCheckIfGameEnded() {
         val game = RootService()
 
         val test = TestRefreshable()
@@ -29,7 +27,20 @@ class CheckIfGameEndedTest {
 
         val tile1 = PathTile(
             mutableMapOf(1 to 2, 3 to 4, 5 to 0), 0, 0,
-            0, mutableListOf()
+            0,
+            mutableListOf(GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE)
+        )
+        val tile2 = TreasureTile(
+            mutableMapOf(1 to 2, 3 to 4, 5 to 0), 0, 0,
+            0,
+            mutableListOf(GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE)
+        )
+        val deque: ArrayDeque<GemType> = ArrayDeque()
+        deque.addAll(mutableListOf(GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE))
+
+        val tile3 = CenterTile(
+            mutableMapOf(1 to 2, 3 to 4, 5 to 0), 0, 0,
+            0, deque
         )
         //check if the assertion are working
         assertThrows<IllegalStateException> { game.gameService.checkIfGameEnded() }
@@ -45,23 +56,24 @@ class CheckIfGameEndedTest {
         val newGame = game.currentGame
         checkNotNull(newGame)
 
-        val gem = GemType.SAPPHIRE
-        repeat(12) {
-            newGame.gateList[0][1].gemsCollected.add(gem)
+
+
+        newGame.gameLayout[0] = mutableListOf(tile1, tile2, tile3)
+        for (i in 1 until newGame.gameLayout.size) {
+            newGame.gameLayout[i] = mutableListOf()
         }
 
-        //check if checkIfGameEnded works after adding 12 gems to the gates
         game.gameService.checkIfGameEnded()
         assertTrue(test.refreshAfterEndGameCalled)
 
     }
 
     /**
-     * Tests whether the game does not end when not all the gems have reached the gates.
-     * Then, it checks if the game ends correctly when all the gems have reached the gates.
+     * Tests whether the game does not end when not all the gems have removed from the filed.
+     * Then, it checks if the game ends correctly when all the gems have removed from the field.
      */
     @Test
-    fun testCheckIfGameEnded1(){
+    fun testCheckIfGameEnded1() {
         val game = RootService()
 
         val test = TestRefreshable()
@@ -69,13 +81,13 @@ class CheckIfGameEndedTest {
         game.addRefreshables(test)
 
 
-        val tile1 = PathTile(
+        val tile = PathTile(
             mutableMapOf(1 to 2, 3 to 4, 5 to 0), 0, 0,
             0, mutableListOf()
         )
         //check if the assertion are working
         assertThrows<IllegalStateException> { game.gameService.checkIfGameEnded() }
-        assertThrows<IllegalStateException> { game.gameService.isPlaceAble(0, 0, tile1) }
+        assertThrows<IllegalStateException> { game.gameService.isPlaceAble(0, 0, tile) }
 
         val player = mutableListOf(
             Player("q", 0, PlayerType.LOCALPLAYER, 0, 6, mutableListOf(), mutableListOf()),
@@ -87,37 +99,46 @@ class CheckIfGameEndedTest {
         val newGame = game.currentGame
         checkNotNull(newGame)
 
-        val gem = GemType.SAPPHIRE
 
-        newGame.gateList[0][1].gemsCollected.add(gem)
-        newGame.gateList[0][2].gemsCollected.add(gem)
-        newGame.gateList[1][3].gemsCollected.add(gem)
-        newGame.gateList[1][3].gemsCollected.add(gem)
-        newGame.gateList[5][2].gemsCollected.add(gem)
-        newGame.gateList[4][1].gemsCollected.add(gem)
-        newGame.gateList[5][3].gemsCollected.add(gem)
-        newGame.gateList[4][2].gemsCollected.add(gem)
-        newGame.gateList[5][1].gemsCollected.add(gem)
-        newGame.gateList[2][1].gemsCollected.add(gem)
+        val tile1 = PathTile(
+            mutableMapOf(1 to 2, 3 to 4, 5 to 0), 0, 0,
+            0,
+            mutableListOf(GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE)
+        )
+        val tile2 = TreasureTile(
+            mutableMapOf(1 to 2, 3 to 4, 5 to 0), 0, 0,
+            0,
+            mutableListOf(GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE)
+        )
+        val deque: ArrayDeque<GemType> = ArrayDeque()
+        deque.addAll(mutableListOf(GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE))
 
-        //check if checkIfGameEnded does not work after adding 10 gems to the gates
+        val tile3 = CenterTile(
+            mutableMapOf(1 to 2, 3 to 4, 5 to 0), 0, 0,
+            0, deque
+        )
+
+        newGame.gameLayout[1] = mutableListOf(tile1)
+        newGame.gameLayout[2] = mutableListOf(tile2)
+        newGame.gameLayout[0] = mutableListOf(tile3)
+
         game.gameService.checkIfGameEnded()
         assertFalse(test.refreshAfterEndGameCalled)
 
-        newGame.gateList[3][2].gemsCollected.add(gem)
-        newGame.gateList[1][0].gemsCollected.add(gem)
+        for (i in 3 until newGame.gameLayout.size) {
+            newGame.gameLayout[i] = mutableListOf(tile1)
+        }
 
-        //check if checkIfGameEnded works after adding 12 gems to the gates
         game.gameService.checkIfGameEnded()
         assertTrue(test.refreshAfterEndGameCalled)
 
     }
 
     /**
-     * Tests if the game does not end when not all the gems have reached the gates.
+     * Tests if the game does not end when not all the gems have removed from the field.
      */
     @Test
-    fun testCheckIfGameNotEnded(){
+    fun testCheckIfGameNotEnded() {
         val game = RootService()
 
         val test = TestRefreshable()
@@ -125,13 +146,13 @@ class CheckIfGameEndedTest {
         game.addRefreshables(test)
 
 
-        val tile1 = PathTile(
+        val tile = PathTile(
             mutableMapOf(1 to 2, 3 to 4, 5 to 0), 0, 0,
             0, mutableListOf()
         )
-        //check if the assertion are working
+
         assertThrows<IllegalStateException> { game.gameService.checkIfGameEnded() }
-        assertThrows<IllegalStateException> { game.gameService.isPlaceAble(0, 0, tile1) }
+        assertThrows<IllegalStateException> { game.gameService.isPlaceAble(0, 0, tile) }
 
         val player = mutableListOf(
             Player("q", 0, PlayerType.LOCALPLAYER, 0, 6, mutableListOf(), mutableListOf()),
@@ -143,14 +164,150 @@ class CheckIfGameEndedTest {
         val newGame = game.currentGame
         checkNotNull(newGame)
 
-        val gem = GemType.SAPPHIRE
-        repeat(11) {
-            newGame.gateList[0][1].gemsCollected.add(gem)
-        }
+        val tile1 = PathTile(
+            mutableMapOf(1 to 2, 3 to 4, 5 to 0), 0, 0,
+            0,
+            mutableListOf(GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE)
+        )
+        val tile2 = TreasureTile(
+            mutableMapOf(1 to 2, 3 to 4, 5 to 0), 0, 0,
+            0,
+            mutableListOf(GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE)
+        )
+        val deque: ArrayDeque<GemType> = ArrayDeque()
+        deque.addAll(mutableListOf(GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE))
 
-        //check if checkIfGameEnded do not work after adding 11 gems to the gates
+        val tile3 = CenterTile(
+            mutableMapOf(1 to 2, 3 to 4, 5 to 0), 0, 0,
+            0, deque
+        )
+
+        newGame.gameLayout[1] = mutableListOf(tile1)
+        newGame.gameLayout[2] = mutableListOf(tile2)
+        newGame.gameLayout[0] = mutableListOf(tile3)
+
         game.gameService.checkIfGameEnded()
         assertFalse(test.refreshAfterEndGameCalled)
 
+        for (i in 3 until newGame.gameLayout.size - 3) {
+            newGame.gameLayout[i] = mutableListOf(tile1)
+        }
+        val tile4 = PathTile(
+            mutableMapOf(1 to 2, 3 to 4, 5 to 0), 0, 0,
+            0,
+            mutableListOf(GemType.NONE, GemType.SAPPHIRE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE)
+        )
+        newGame.gameLayout[newGame.gameLayout.size - 2] = mutableListOf(tile4)
+
+        game.gameService.checkIfGameEnded()
+        assertFalse(test.refreshAfterEndGameCalled)
+
+
     }
-}
+
+    /**
+     * Tests if the game does not end when not all the tiles have been placed on the field.
+     */
+    @Test
+    fun testCheckIfGameNotEnded1() {
+        val game = RootService()
+
+        val test = TestRefreshable()
+        test.reset()
+        game.addRefreshables(test)
+
+
+        val tile = PathTile(
+            mutableMapOf(1 to 2, 3 to 4, 5 to 0), 0, 0,
+            0, mutableListOf()
+        )
+        //check if the assertion are working
+        assertThrows<IllegalStateException> { game.gameService.checkIfGameEnded() }
+        assertThrows<IllegalStateException> { game.gameService.isPlaceAble(0, 0, tile) }
+
+        val player = mutableListOf(
+            Player("q", 0, PlayerType.LOCALPLAYER, 0, 6, mutableListOf(), mutableListOf()),
+            Player("q1", 0, PlayerType.LOCALPLAYER, 0, 6, mutableListOf(), mutableListOf())
+        )
+
+        game.gameService.startNewGame(player, false, 1.0, false)
+
+        val newGame = game.currentGame
+        checkNotNull(newGame)
+
+
+        val deque: ArrayDeque<GemType> = ArrayDeque()
+        deque.addAll(mutableListOf(GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE))
+
+
+
+
+            newGame.gameLayout[newGame.gameLayout.size - 1] = mutableListOf(
+                EmptyTile(mutableMapOf(), 0, 9, 0)
+            )
+
+
+        game.gameService.checkIfGameEnded()
+        assertFalse(test.refreshAfterEndGameCalled)
+
+
+    }
+
+    /**
+     * Tests if the game does not end when all the tiles have been placed on the field.
+     */
+    @Test
+    fun testCheckIfGameEnded2() {
+        val game = RootService()
+
+        val test = TestRefreshable()
+        test.reset()
+        game.addRefreshables(test)
+
+
+        val tile = PathTile(
+            mutableMapOf(1 to 2, 3 to 4, 5 to 0), 0, 0,
+            0, mutableListOf()
+        )
+        //check if the assertion are working
+        assertThrows<IllegalStateException> { game.gameService.checkIfGameEnded() }
+        assertThrows<IllegalStateException> { game.gameService.isPlaceAble(0, 0, tile) }
+
+        val player = mutableListOf(
+            Player("q", 0, PlayerType.LOCALPLAYER, 0, 6, mutableListOf(), mutableListOf()),
+            Player("q1", 0, PlayerType.LOCALPLAYER, 0, 6, mutableListOf(), mutableListOf())
+        )
+
+        game.gameService.startNewGame(player, false, 1.0, false)
+
+        val newGame = game.currentGame
+        checkNotNull(newGame)
+
+        val tile1 = PathTile(
+            mutableMapOf(1 to 2, 3 to 4, 5 to 0), 0, 0,
+            0,
+            mutableListOf(GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE)
+        )
+        val tile2 = TreasureTile(
+            mutableMapOf(1 to 2, 3 to 4, 5 to 0), 0, 0,
+            0,
+            mutableListOf(GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE)
+        )
+        val deque: ArrayDeque<GemType> = ArrayDeque()
+        deque.addAll(mutableListOf(GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE, GemType.NONE))
+
+        val tile3 = CenterTile(
+            mutableMapOf(1 to 2, 3 to 4, 5 to 0), 0, 0,
+            0, deque
+        )
+
+        for (i in 0 until newGame.gameLayout.size ) {
+            newGame.gameLayout[i] = mutableListOf(tile1 , tile2 , tile3)
+        }
+
+            game.gameService.checkIfGameEnded()
+            assertTrue(test.refreshAfterEndGameCalled)
+
+        }
+
+    }
