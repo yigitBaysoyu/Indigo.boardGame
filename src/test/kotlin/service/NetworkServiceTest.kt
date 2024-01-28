9e2187b44080fa69a4f1208086a312f626a83c22
kusaynaeem <94133560+kusaynaeem@users.noreply.github.com>
2024-01-28 02:42:16 +0100

    Documentation from German to English

--- src/test/kotlin/service/NetworkServiceTest.kt
+++ src/test/kotlin/service/NetworkServiceTest.kt
@@ -1,17 +1,14 @@
 package service
 
-import entity.Player
+
 import entity.PlayerType
 import kotlin.test.assertNotNull
 import edu.udo.cs.sopra.ntf.*
 import kotlin.random.Random
 import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 
-import kotlin.test.assertNull
 /**
- * Testfälle für die Klasse NetWorkService
+ * Test cases for the NetworkService class.
  */
 class NetworkServiceTest {
 
@@ -26,8 +23,8 @@ class NetworkServiceTest {
 
 
     /**
-     * Initialisieren Sie beide Verbindungen und starten Sie das Spiel, damit die Spieler beider Spiele
-     * (dargestellt durch [rootServiceHost] und [rootServiceGuest]) in ihren Zügen sind.
+     * Initialize both connections and start the game so that players of both games
+     * (represented by [rootServiceHost] and [rootServiceGuest]) are in their turns.
      */
     private fun initConnections() {
 
@@ -38,25 +35,41 @@ class NetworkServiceTest {
 
 
 
-        rootServiceHost.networkService.hostGame(NETWORK_SECRET,  generateRandomNumberAsString(),"ahmad", color = PlayerColor.WHITE ,GameMode.THREE_NOT_SHARED_GATEWAYS)
+        rootServiceHost.networkService.hostGame(
+            NETWORK_SECRET,
+            generateRandomNumberAsString(),
+            "ahmad",
+            color = PlayerColor.WHITE,
+            GameMode.THREE_NOT_SHARED_GATEWAYS
+        )
 
-        assert(rootServiceHost.waitForState(ConnectionState.WAITING_FOR_GUESTS)){
+        assert(rootServiceHost.waitForState(ConnectionState.WAITING_FOR_GUESTS)) {
             error("Nach dem Warten nicht im Zustand angekommen")
         }
 
         val hostClient = rootServiceHost.networkService.client
         assertNotNull(hostClient)
 
-        rootServiceGuest.networkService.joinGame(NETWORK_SECRET, hostClient.sessionID!!,"mohmed",PlayerType.NETWORKPLAYER)
-        assert(rootServiceGuest.waitForState(ConnectionState.WAITING_FOR_INIT)){
+        rootServiceGuest.networkService.joinGame(
+            NETWORK_SECRET,
+            hostClient.sessionID!!,
+            "mohmed",
+            PlayerType.NETWORKPLAYER
+        )
+        assert(rootServiceGuest.waitForState(ConnectionState.WAITING_FOR_INIT)) {
             error("Nach dem Warten nicht im Zustand angekommen")
         }
 
 
 
-        rootServiceGuest2.networkService.joinGame(NETWORK_SECRET, hostClient.sessionID!!,"Alex",PlayerType.NETWORKPLAYER)
+        rootServiceGuest2.networkService.joinGame(
+            NETWORK_SECRET,
+            hostClient.sessionID!!,
+            "Alex",
+            PlayerType.NETWORKPLAYER
+        )
 
-        assert(rootServiceGuest2.waitForState(ConnectionState.WAITING_FOR_INIT)){
+        assert(rootServiceGuest2.waitForState(ConnectionState.WAITING_FOR_INIT)) {
             error("Nach dem Warten nicht im Zustand angekommen")
         }
 
@@ -68,7 +81,7 @@ class NetworkServiceTest {
     }
 
     /**
-     * Test für Host und Beitritt zum neuen Spiel
+     * Test for hosting and joining a new game.
      */
     @Test
     fun testHostAndJoinGame() {
@@ -77,20 +90,15 @@ class NetworkServiceTest {
         initConnections()
 
 
-
-
-
-
-
-
-
-
-
-
-        }
     }
 
-    private fun RootService.waitForState(state: ConnectionState, timeout: Int = 5000):Boolean {
+    /**
+     * Waits for the specified state in the rootService's NetworkService connection state.
+     * @param state The target ConnectionState to wait for.
+     * @param timeout The maximum duration, in milliseconds, to wait for the target state
+     * @return True if the target state is reached within the specified timeout, otherwise false.
+     */
+    private fun RootService.waitForState(state: ConnectionState, timeout: Int = 5000): Boolean {
         var timePassed = 0
         while (timePassed < timeout) {
             if (networkService.connectionState == state)
@@ -103,7 +111,11 @@ class NetworkServiceTest {
         return false
     }
 
-    fun generateRandomNumberAsString(): String {
+    /**
+     * Generates a random number and converts it to a String.
+     * @return The generated random number as a String.
+     */
+    private fun generateRandomNumberAsString(): String {
         // Define the range for the random number.
         val lowerBound = 2001
         val upperBound = Int.MAX_VALUE
@@ -111,6 +123,7 @@ class NetworkServiceTest {
         // Generate a random number within the defined range and convert it to a String.
         return Random.nextInt(lowerBound, upperBound).toString()
     }
+}
 
 
 
